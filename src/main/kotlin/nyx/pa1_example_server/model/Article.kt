1deb7f152c7641946e4046258bccee772b19dc6a
Felix Huber <felix.huber@ametras.com>
2021-09-17 09:35:26 +0200

    minor stuff

--- src/main/kotlin/nyx/pa1_example_server/model/Article.kt
+++ src/main/kotlin/nyx/pa1_example_server/model/Article.kt
@@ -1,7 +1,11 @@
 package nyx.pa1_example_server.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ArticlePreview(val id:Int, val title:String, val imageUrl:String)
 
+@Serializable
 data class ArticleDetail(val id:Int, val title:String, val imageUrl:String, val publishTimestamp:Long, val text:String)
 
 fun ArticlePreview.addDetail(publishTimestamp: Long, text: String) = ArticleDetail(id, title, imageUrl, publishTimestamp,text)
